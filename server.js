2456dfaad461786c8f7e17b63b0a5c9f390c287e
Leart Krasniqi <leart.krasniqi@prosperoware.com>
2022-01-12 23:09:34 +0100

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const req = require('express/lib/request');
+const { rawListeners } = require('process');
 const app = express();
 
 // Basic Configuration
+const options = {
+  family: 4,
+  hints: dns.ADDRCONFIG | dns.V4MAPPED,
+};
 const port = process.env.PORT || 3000;
-
+app.use(express.urlencoded({extended : false}))
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use((req,res,next) => {
+  if(isURL(req.body.url)){ 
+    console.log(typeof req.body.url)
+    console.log(req.body.url);
+    dns.lookup(req.body.url,options , (err, address, family) => {
+      if(err){
+        console.log(err);
+        next();
+      } else {
+        console.log
+        req.body.urlshort = address;
+        next();
+      }
+    })
+  } else {
+    req.body.urlshort = 'invalid url'
+  }
+})
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,7 +42,24 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.post('/api/shorturl', (req,res) => {
+    if(req.body.urlshort === "invalid url"){
+      res.json({error : "invalid url"});
+    } else{ 
+      res.json({original_url : req.body.url, short_url : req.body.urlshort});
+    }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+function isURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
+}
